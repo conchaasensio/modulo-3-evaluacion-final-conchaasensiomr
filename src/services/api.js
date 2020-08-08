19e7b6a651dbeed5dc6940298470baf3aa400ab8
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-08 12:42:58 +0200

    refactor extract method in Api

--- src/services/api.js
+++ src/services/api.js
@@ -4,14 +4,14 @@ const getDataFromApi = () => {
   return fetch('https://rickandmortyapi.com/api/character/')
     .then((response) => response.json())
     .then((data) => data.results)
-    .then((characters) => {
-      return characters.map((character) => {
-        character.species = character.species === 'Human' ? 'Humano' : 'Alien';
-        character.status = character.status === 'Alive' ? 'Vivo' : 'Muerto';
-
-        return character;
-      });
-    });
+    .then((characters) => characters.map(translateCharacterInfo));
 };
+
+function translateCharacterInfo(character) {
+  character.species = character.species === 'Human' ? 'Humano' : 'Alien';
+  character.status = character.status === 'Alive' ? 'Vivo' : 'Muerto';
+
+  return character;
+}
 
 export default getDataFromApi;
