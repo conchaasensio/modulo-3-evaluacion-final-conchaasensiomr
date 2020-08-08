fa3bb13a3d21a134eb9a1ee0b4fbfd6abd4553a3
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-08 08:58:54 +0200

    add content to Filters component

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class componentName extends Component {
-  render() {
-    return <div></div>;
-  }
-}
+const Filters = () => {
+  return (
+    <form action="">
+      <div>
+        <label htmlFor="filterName">Busca tu personaje</label>
+        <input type="text" id="filterName" />
+      </div>
+      <div>
+        <label htmlFor="filterSpecie">Busca por especie</label>
+        <select name="filterSpecie" id="filterSpecie">
+          <option value="all">Todos</option>
+          <option value="human">Human</option>
+          <option value="alien">Alien</option>
+        </select>
+      </div>
+    </form>
+  );
+};
 
-export default componentName;
+export default Filters;
