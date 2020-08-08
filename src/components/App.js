0863730a2b9efe9593e8c46c01935c4360de3c81
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-08 11:34:42 +0200

    add sort function to alphabetizing both characters rendered and  search results

--- src/components/App.js
+++ src/components/App.js
@@ -49,6 +49,15 @@ function App() {
 
   const renderFilteredCharacters = () => {
     return characters
+      .sort(function (a, b) {
+        if (a.name.toLowerCase() > b.name.toLowerCase()) {
+          return 1;
+        }
+        if (a.name.toLowerCase() < b.name.toLowerCase()) {
+          return -1;
+        }
+        return 0;
+      })
       .filter((character) => {
         return character.name.toLowerCase().includes(filterName.toLowerCase());
       })
