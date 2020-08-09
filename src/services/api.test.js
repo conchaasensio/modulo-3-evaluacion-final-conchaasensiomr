4f8f7d25565766ea1fd41ad8f51a379cc035dca8
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-09 17:31:48 +0200

    add test selectRelevantData contains requested data

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -4,6 +4,21 @@ const getDataFromApi = require('./api');
 describe('getDataFromApi', () => {
   test('return 60 characters', async () => {
     const characters = await getDataFromApi();
+
     expect(characters.length).toBe(60);
+  });
+
+  test('selectRelevantData', async () => {
+    const characters = await getDataFromApi();
+
+    expect(Object.keys(characters[0])).toContain('id');
+    expect(Object.keys(characters[0])).toContain('image');
+    expect(Object.keys(characters[0])).toContain('name');
+    expect(Object.keys(characters[0])).toContain('status');
+    expect(Object.keys(characters[0])).toContain('specie');
+    expect(Object.keys(characters[0])).toContain('origin');
+    expect(Object.keys(characters[0])).toContain('episodes');
+    expect(Object.keys(characters[0])).toContain('gender');
+    expect(Object.keys(characters[0]).length).toBe(8);
   });
 });
