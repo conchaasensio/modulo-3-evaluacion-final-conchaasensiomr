fd469cd5a9aeddc8f046c7857ed1519d57f9333d
Concha Asensio <conchaasensiomr@gmail.com>
2020-08-09 08:39:15 +0200

    add scss files and reset.scss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import './index.scss';
+// import './index.scss';
 import App from './components/App';
 
 ReactDOM.render(
